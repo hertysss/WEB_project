2c61794e2582c7afd77327095032b6937e2ff22d
streizz <88502728+streizz@users.noreply.github.com>
2023-04-03 18:24:54 +0300

    123

--- data/__all_models.py
+++ data/__all_models.py
@@ -1,1 +1,5 @@
 from . import users
+from . import landlenders
+from . import objects
+from . import offices
+from . import renters
--- data/users.py
+++ data/users.py
@@ -1,5 +1,3 @@
-import datetime
-
 import sqlalchemy
 from data.db_session import SqlAlchemyBase
 
@@ -9,12 +7,7 @@ class User(SqlAlchemyBase):
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
-    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
-                                      default=datetime.datetime.now)
-
-    def __repr__(self):
-        return (f'{self.name} {self.surname} {self.email} {self.hashed_password}')
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=False)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    role = sqlalchemy.Column(sqlalchemy.String, nullable=False)
