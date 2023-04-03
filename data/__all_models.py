873120223aef483e43ddeb47e5d77c406b12889b
streizz <88502728+streizz@users.noreply.github.com>
2023-04-03 23:06:45 +0300

    Revert "models"

--- data/__all_models.py
+++ data/__all_models.py
@@ -1,5 +1,1 @@
 from . import users
-from . import landlenders
-from . import objects
-from . import offices
-from . import renters
--- data/users.py
+++ data/users.py
@@ -1,3 +1,5 @@
+import datetime
+
 import sqlalchemy
 from data.db_session import SqlAlchemyBase
 
@@ -7,7 +9,12 @@ class User(SqlAlchemyBase):
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=False)
-    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    role = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                      default=datetime.datetime.now)
+
+    def __repr__(self):
+        return (f'{self.name} {self.surname} {self.email} {self.hashed_password}')
