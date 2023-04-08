69cda1ad449fa1e05846c3fbdde548427ba781f4
streizz <n.kananadze8@gmail.com>
2023-04-08 22:11:13 +0300

    new file:   data/offices_api.py

---
+++ data/offices_api.py
@@ -0,0 +1,34 @@
+from flask import jsonify
+import flask
+import sqlalchemy.orm as orm
+from db_session import create_session
+from __all_models import Office
+
+blueprint = flask.Blueprint(
+    'offices_api',
+    __name__,
+    template_folder='templates'
+)
+
+
+@blueprint.route('/offices')
+def get_offices():
+    session = create_session()
+
+    office = session.query(Office).filter(Office.status == False).options(
+        orm.joinedload(Office.business_center)).all()
+
+    res = []
+    for office in office:
+        office_dict = {
+            'Площадь': office.area,
+            'Этаж': office.floor,
+            'Ставка': office.bet,
+            'Название бизнес центра': office.business_center.name,
+            'Адресс бизнес центра': office.business_center.address
+        }
+        res.append(office_dict)
+
+    session.close()
+
+    return jsonify(res)
